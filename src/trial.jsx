ffd72b0757114f662e36e38a54c4297756d29e9e
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-01-16 00:45:29 +0100

    deleted a file

--- src/trial.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-export default function Ant(){
-    const [aoo, setAoo]= useState([])
-    setAoo("mw")
-    console.log(aoo)
-    setAoo((prev)=> [...prev, "rydcn"])
-    console.log(setAoo)
-}
